6fdf2d5abb9f43a461eae29f34e7875a01fcc23c
Miillky <vedran.milkovic25@gmail.com>
2020-03-28 19:18:09 +0100

    Zadatak 5

---
+++ vjezba3/zadatak5.php
@@ -0,0 +1,83 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport"
+	      content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+	<title>Zadatak 5</title>
+</head>
+<body>
+
+<?php
+$predmeti = [
+	'Odaberite predmet',
+	'Mikroračunala',
+	'Programsko inženjerstvo',
+	'Operacijski sustavi',
+	'Osnove programiranja',
+	'C# Programiranje',
+	'Web programiranje',
+	'Tehnički engleski 4'
+];
+
+$ocjene = [
+	1 => 'nedovoljan 1',
+	2 => 'dovoljan 2',
+	3 => 'dobar 3',
+	4 => 'vrlo dobar 4',
+	5 => 'odličam 5'
+];
+
+$studenti = ['vmilkovic'];
+
+?>
+
+<style>
+	button,
+	select {
+		display: block;
+		margin: 10px;
+	}
+</style>
+
+<form action="">
+	<label for="username">Unesite username:</label>
+	<input id="username" type="text" name="username" value="<?php echo $_GET['username']; ?>">
+	<select name="predmet">
+		<?php foreach( $predmeti as $index => $predmet ): ?>
+			<option value="<?php echo $index; ?>" <?php echo ($_GET['predmet'] == $index) ? 'selected' : ''?>><?php echo $predmet; ?></option>
+		<?php endforeach; ?>
+	</select>
+	<?php foreach( $ocjene as $oindex => $ocjena ): ?>
+		<input id="ocjena-<?php echo $oindex; ?>" type="radio" name="ocjena" value="<?php echo $oindex;?>" <?php echo ($_GET['ocjena'] == $oindex) ? 'checked' : ''; ?>/>
+		<label for="ocjena-<?php echo $oindex; ?>"><?php echo $ocjena; ?></label><br>
+	<?php endforeach; ?>
+	<button type="Submit">Pošalji</button>
+</form>
+
+<?php if( !$_GET['predmet'] && isset($_GET['predmet']) ): ?>
+	<script>
+        alert("Molimo odaberite predmet!")
+	</script>
+<?php endif; ?>
+
+<?php if ( !$_GET['ocjena'] && isset($_GET['ocjena']) ): ?>
+	<script>
+        alert("Molimo unsetie ocjenu!")
+	</script>
+<?php endif; ?>
+
+<?php if( $_GET['username'] && !in_array( $_GET['username'], $studenti ) ): ?>
+	<script>
+        alert("Ne postoji korisnik s imenom <?php echo $_GET['username']; ?>!")
+	</script>
+<?php endif; ?>
+
+<?php if( $_GET['ocjena'] && $_GET['predmet'] && $_GET['username'] && in_array( $_GET['username'], $studenti ) ): ?>
+	<script>
+        alert("<?php echo $_GET['username'] . ' je na predmetu ' . $predmeti[$_GET['ocjena']] . ' dobio ' . $ocjene[$_GET['ocjena']]; ?>")
+	</script>
+<?php endif; ?>
+</body>
+</html>
